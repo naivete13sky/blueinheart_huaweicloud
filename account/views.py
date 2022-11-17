df14a508390acac4c5ac649cee1e1ac78d85a7e5
naivete13sky <chen320821@163.com>
2022-11-17 16:56:45 +0800

    20221117 24 account messages

--- account/views.py
+++ account/views.py
@@ -3,6 +3,7 @@ from django.contrib.auth import authenticate, login
 from .forms import LoginForm,UserRegistrationForm, UserEditForm, ProfileEditForm
 from django.contrib.auth.decorators import login_required
 from .models import Profile
+from django.contrib import messages
 
 
 #做个试验，后面不用这个了
@@ -73,6 +74,10 @@ def edit(request):
         if user_form.is_valid() and profile_form.is_valid():
             user_form.save()
             profile_form.save()
+            #为视图增加了两条语句，分别在成功登录之后显示成功信息，在表单验证失败的时候显示错误信息。
+            messages.success(request, 'Profile updated successfully')
+        else:
+            messages.error(request, "Error updating your profile")
     else:
         user_form = UserEditForm(instance=request.user)
         profile_form = ProfileEditForm(instance=request.user.profile)
