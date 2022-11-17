a56164ff28fff14b583398b65963f337634a40e1
naivete13sky <chen320821@163.com>
2022-11-17 14:17:22 +0800

    20221117 10 tags

--- blog/views.py
+++ blog/views.py
@@ -4,9 +4,18 @@ from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
 from django.views.generic import ListView
 from django.core.mail import send_mail
 from .forms import EmailPostForm, CommentForm
+from taggit.models import Tag
 
-def post_list(request):
+def post_list(request,tag_slug=None):
     object_list = Post.published.all()
+
+    tag = None
+    if tag_slug:
+        tag = get_object_or_404(Tag, slug=tag_slug)
+        object_list = object_list.filter(tags__in=[tag])
+
+
+
     paginator = Paginator(object_list, 3)  # 每页显示3篇文章
     page = request.GET.get('page')
     try:
@@ -17,7 +26,7 @@ def post_list(request):
     except EmptyPage:
         # 如果页数超出总页数就返回最后一页
         posts = paginator.page(paginator.num_pages)
-    return render(request, 'blog/post/list.html', {'page': page, 'posts': posts})
+    return render(request, 'blog/post/list.html', {'page': page, 'posts': posts, 'tag': tag})
 
 
 class PostListView(ListView):
