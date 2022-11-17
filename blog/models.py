c9c86834e7decc8b15a7adb41a3dbffe65fa8911
naivete13sky <chen320821@163.com>
2022-11-17 11:50:46 +0800

    20221117 5

--- blog/models.py
+++ blog/models.py
@@ -1,7 +1,11 @@
 from django.db import models
 from django.utils import timezone
 from django.contrib.auth.models import User
+from django.urls import reverse
 
+class PublishedManager(models.Manager):
+    def get_queryset(self):
+        return super(PublishedManager, self).get_queryset().filter(status='published')
 
 class Post(models.Model):
     STATUS_CHOICES = (('draft', 'Draft'), ('published', 'Published'))
@@ -21,3 +25,9 @@ class Post(models.Model):
 
     def __str__(self):
         return self.title
+
+    objects = models.Manager()  # 默认的管理器
+    published = PublishedManager()  # 自定义管理器
+
+    def get_absolute_url(self):
+        return reverse('blog:post_detail', args=[self.publish.year, self.publish.month, self.publish.day, self.slug])
