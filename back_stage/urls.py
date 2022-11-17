8b0d16bcf756520138d6f4171a9a5826cf8dd1bc
naivete13sky <chen320821@163.com>
2022-11-17 20:45:08 +0800

    20221117 28 ok

--- back_stage/urls.py
+++ back_stage/urls.py
@@ -4,6 +4,7 @@ from . import views
 app_name = 'back_stage'
 urlpatterns = [
     path('', views.index, name='index'),
+    path('index', views.index, name='index2'),
     # path('dashboard', views.dashboard, name='dashboard'),
     # path('index_base_20220818', views.index_base_20220818, name='index_base_20220818'),
     # path('DashBoardView',views.DashBoardView.as_view(),name='DashBoardView')
