ab5e18cef72d3b64821e0d432e5b342714dc8afb
naivete13sky <chen320821@163.com>
2022-11-17 18:18:19 +0800

    20221117 25 ok

--- blueinheart/settings.py
+++ blueinheart/settings.py
@@ -25,7 +25,8 @@ SECRET_KEY = 'django-insecure-tr&c99#!@tn!gepa&%t$b=rgg)dj!(5dz57130n3k!kt%&yz4q
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+# ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 SITE_ID = 1
