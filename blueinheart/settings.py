e4fe9b02fc219260f8088b7c8023128e4ba65fb5
naivete13sky <chen320821@163.com>
2022-11-17 10:49:38 +0800

    20221117 数据库改为pg

--- blueinheart/settings.py
+++ blueinheart/settings.py
@@ -74,10 +74,25 @@ WSGI_APPLICATION = 'blueinheart.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.0/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'dms',
+        # 'NAME': 'ep_develop',
+        'USER': 'postgres',
+        'PASSWORD': 'cc',
+        # 'HOST': '10.97.80.118',
+        'HOST': '127.0.0.1',
+        # 'HOST': '10.97.80.147',
+
+        'PORT': '5432',
     }
 }
 
